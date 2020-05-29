170230af142aad1a28e65fbd6b3b648ed870946d
Jitgitbit <rowowolf@gmail.com>
2020-05-29 07:05:44 +0200

    Implementing Basic Routing

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const placesRoutes = require('./routes/places-routes');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on: ${port}, endpoint /... is supported`));  // start Node + Express server on port 5000
+app.use(placesRoutes);
+
+app.listen(port, () => console.log(`Listening on: ${port}, endpoint /testing is supported!`));  // start Node + Express server on port 5000
