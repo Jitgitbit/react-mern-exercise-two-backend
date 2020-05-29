1df146ed7977221c69ad6a1bf73a08956b4713e5
Jitgitbit <rowowolf@gmail.com>
2020-05-29 20:31:04 +0200

    Handling Errors for Unsupported Routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,11 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
                                          //------> the sequence of ANY middleWare is of major importance !!! They will be parsed from top to bottom !!!
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route', 404);
+  throw error;                          //-----------> throw error because now it's still synchronous !
+});
 
 app.use((error, req, res, next) => {
   if(res.headerSent){
