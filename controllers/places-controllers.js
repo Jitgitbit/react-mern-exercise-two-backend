f3f5f7d6f796b3764e4d7a0aa246034034452651
Jitgitbit <rowowolf@gmail.com>
2020-05-30 23:56:00 +0200

    cleaned up

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,9 +1,8 @@
 const {uuid} = require('uuidv4');
-const { validationResult } = require("express-validator");
+const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
 const getCoordsForAddress = require('../util/location');
-
 
 let DUMMY_PLACES = [
   {
@@ -45,7 +44,7 @@ const getPlacesByUserId = (req, res, next) => {
 
   if (!places || places.length === 0) {
     return next(
-      new HttpError('Could not find any places for the provided user id.', 404)
+      new HttpError('Could not find places for the provided user id.', 404)
     );
   }
 
@@ -54,62 +53,60 @@ const getPlacesByUserId = (req, res, next) => {
 
 const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    console.log(errors);
-    next(new HttpError('Invalid inputs given, please check your data!', 422));
+  if (!errors.isEmpty()) {
+    next(new HttpError('Invalid inputs passed, please check your data.', 422));
   }
 
-  const {title, description, creator, address} = req.body;             //-------> good object destructuring !
-  // above is short for doing:  const title = req.body.title , etc for each property !
+  const { title, description, address, creator } = req.body;
 
   let coordinates;
-  try{
+  try {
     coordinates = await getCoordsForAddress(address);
-  }catch(error){
+  } catch (error) {
     return next(error);
   }
 
+  // const title = req.body.title;
   const createdPlace = {
     id: uuid(),
     title,
     description,
-    location: coordinates,                         //-------> nice object literal !
+    location: coordinates,
     address,
     creator
   };
 
-  DUMMY_PLACES.push(createdPlace);   // DUMMY_PLACES.unshift(createdPlace);
+  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
 
-  res.status(201).json({place: createdPlace});
+  res.status(201).json({ place: createdPlace });
 };
 
 const updatePlace = (req, res, next) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    throw new HttpError('Invalid inputs given, please check your data!', 422)
+  if (!errors.isEmpty()) {
+    throw new HttpError('Invalid inputs passed, please check your data.', 422);
   }
-  const {title, description} = req.body;             //-------> good object destructuring !
-  const placeId = req.params.pid;
-  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId)};      //----> clever use of the spread operator !
-  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
 
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
   updatedPlace.title = title;
   updatedPlace.description = description;
 
-  DUMMY_PLACES[placeIndex] = updatedPlace;    
+  DUMMY_PLACES[placeIndex] = updatedPlace;
 
-  res.status(200).json({place: updatedPlace});
+  res.status(200).json({ place: updatedPlace });
 };
 
 const deletePlace = (req, res, next) => {
-  const placeId = rq.params.pid;
-
-  if(!DUMMY_PLACES.find(p => p.id === placeId)){
+  const placeId = req.params.pid;
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
     throw new HttpError('Could not find a place for that id.', 404);
   }
-
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
-  res.status(200).json({message: 'Deleted place.'});
+  res.status(200).json({ message: 'Deleted place.' });
 };
 
 exports.getPlaceById = getPlaceById;
