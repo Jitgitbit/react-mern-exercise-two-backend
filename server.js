d60204bcb7cc4e0850cc4030d844d1f87f830231
Jitgitbit <rowowolf@gmail.com>
2020-06-03 13:49:17 +0200

    Handling CORS without the cors() package !

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ const HttpError = require('./models/http-error');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-                                         //------> the sequence of ANY middleWare is of major importance !!! They will be parsed from top to bottom !!!
+app.use(bodyParser.json());         //------> the sequence of ANY middleWare is of major importance !!! They will be parsed from top to bottom !!!
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+    
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
