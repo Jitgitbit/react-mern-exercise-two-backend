5cfd5efd2a8ca8c8501930ef33a4aa8afe72ca34
Jitgitbit <rowowolf@gmail.com>
2020-06-03 08:29:23 +0200

    fixed typo, signing in works !

--- models/user.js
+++ models/user.js
@@ -8,7 +8,7 @@ const userSchema = new Schema({
   name: {type: String, required: true},
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true, minlength: 6},
-  address: {type: String, required: true},
+  image: {type: String, required: true},
   places: {type: String, required: true},
 });
 
